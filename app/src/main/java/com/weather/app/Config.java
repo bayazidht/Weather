2d3e804a0e00bfc83f032d04419a19fc428146e5
bayazidht <bayazidht@hotmail.com>
2025-01-06 17:44:38 +0600

    Initial API request

---
+++ app/src/main/java/com/weather/app/Config.java
@@ -0,0 +1,7 @@
+package com.weather.app;
+
+public class Config {
+
+    public final static String API_URL = "https://api.openweathermap.org/data/2.5/weather?";
+    public final static String API_KEY = "5ef082688bf1a43cbfebd24d96104255";
+}
--- app/src/main/java/com/weather/app/MainActivity.java
+++ app/src/main/java/com/weather/app/MainActivity.java
@@ -1,6 +1,7 @@
 package com.weather.app;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +9,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv_visibility, tv_temp, tv_feels_like;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,36 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        tv_visibility = findViewById(R.id.tv_visibility);
+        tv_temp = findViewById(R.id.tv_temp);
+        tv_feels_like = findViewById(R.id.tv_feels_like);
+
+        requestWeather(33.44, -94.04);
+    }
+
+    private void requestWeather(double latitude, double longitude) {
+        String url = Config.API_URL+"lat="+latitude+"&lon="+longitude+"&appid="+Config.API_KEY;
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        tv_visibility.setText("Response: " + response.toString());
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        tv_visibility.setText(error.getMessage());
+                    }
+                });
+
+        queue.add(jsonObjectRequest);
+
     }
 }
